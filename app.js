607afba47406a6c7c7f8f74692dbfdaae38e58cb
chips556 <lohqihern@hotmail.com>
2021-12-17 17:00:25 +0800

    Added single round func

--- app.js
+++ app.js
@@ -2,10 +2,43 @@ function computerPlay() {
     let randNum = Math.floor(Math.random() * 3)
     switch (randNum) {
         case 0:
-            return 'Rock'
+            return 'rock'
         case 1:
-            return 'Paper'
+            return 'paper'
         case 2:
-            return 'Scissors'
+            return 'scissors'
+    }
+}
+
+function round(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case 'rock':
+            switch(computerSelection) {
+                case 'rock':
+                return 'Tie'
+                case 'paper':
+                return 'Paper beats Rock, you lose!'
+                case 'scissors':
+                return 'Rock beats Scissors, you win!'
+            }
+        case 'paper':
+            switch(computerSelection) {
+                case 'rock':
+                return 'Paper beats Rock, you win!'
+                case 'paper':
+                return 'Tie'
+                case 'scissors':
+                return 'Scissors beats papers, you lose!'
+            }
+        case 'scissors':
+            switch(computerSelection) {
+                case 'rock':
+                return 'Rock beats Scissors, you lose!'
+                case 'paper':
+                return 'Scissors beats Paper, you win!'
+                case 'scissors':
+                return 'Tie'
+            }
+
     }
 }
